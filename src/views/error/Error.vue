e57cabc1b278fcb11188b2e71e7b40c95607ec18
xuwei <xuwei@huox.tv>
2018-05-22 15:50:48 +0800

    修复error页面报错

--- src/views/error/Error.vue
+++ src/views/error/Error.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="error">
         <div class="error-body-con">
-            <Card>
-                <img src="../../assets/no_auth.jpg" alt="">
-                <p class="error-body-con-message">Sorry，没有找到您需要的页面！</p>
-                <div class="error-btn-con">
-                    <button @click="backPage">返回上一页</button>
-                </div>
-            </Card>
+            <img src="../../assets/no_auth.jpg" alt="">
+            <p class="error-body-con-message">Sorry，没有找到您需要的页面！</p>
+            <div class="error-btn-con">
+                <button @click="backPage">返回上一页</button>
+            </div>
         </div>
     </div>
 </template>
